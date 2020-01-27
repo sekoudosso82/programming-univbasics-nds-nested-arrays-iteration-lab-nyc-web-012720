494528b901e2109dc842da839bfc7b9fe61b7ea6
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-27 01:26:13 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,49 +1,29 @@
 
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
   
-outer_results = []
-row_index = 0
-while row_index < src.count do
-  
-  outer_results.push ("I love "+ (src[row_index][0]) +" and "+ (src[row_index][1])+" on my pizza")
-  row_index += 1
-end
-outer_results
-    
-  
+    outer_results = []
+    row_index = 0
+    while row_index < src.count do
+      outer_results.push ("I love "+ (src[row_index][0]) +" and "+ (src[row_index][1])+" on my pizza")
+      row_index += 1
+    end
+    outer_results
+
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
   
-  result = []
-  index = 0
-  
-  while index < src.count do
-    src[index][0]> src[index][1] ? result.push(src[index][0]) : result.push(src[index][1])
-    index += 1
-  end
-  result
+    result = []
+    index = 0
+    
+    while index < src.count do
+      src[index][0]> src[index][1] ? result.push(src[index][0]) : result.push(src[index][1])
+      index += 1
+    end
+    result
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
-  
   
   total = 0
   index = 0
@@ -56,5 +36,6 @@ def total_even_pairs(src)
     index += 1
   end
   total
+  
 end
 
